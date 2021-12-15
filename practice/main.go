bf6c162944132345946c257810d420f38b3d40bd
Madhusudan Sridharan <madhusudan.sridharan@q2ebanking.com>
2021-12-14 19:45:55 -0600

    string conversion

--- practice/main.go
+++ practice/main.go
@@ -4,6 +4,8 @@ import (
 	"bufio"
 	"fmt"
 	"os"
+	"strconv"
+	"strings"
 )
 
 func main() {
@@ -12,5 +14,13 @@ func main() {
 	fmt.Print("Enter text: ")
 	input, _ := reader.ReadString('\n')
 	fmt.Println("You entered:", input)
+	fmt.Print("Enter a number: ")
+	inputNumer, _ := reader.ReadString('\n')
+	aNumber, err := strconv.ParseFloat(strings.TrimSpace(inputNumer), 64)
+	if err != nil {
+		fmt.Println("Encountered an error: ", err)
+	} else {
+		fmt.Println("The number is : ", aNumber)
+	}
 
 }
